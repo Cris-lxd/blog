515fbecb9cfc7ca507d473a1c8fbcfdcda371e0d
Cris-lxd <woaini1120620371@gmail.com>
2020-09-30 10:57:50 +0800

    修改当服务器重启后图片路径失效的问题，在webconfig中将虚拟路径请求变为寻找真实路径

--- src/main/java/com/lxd/interceptor/WebConfig.java
+++ src/main/java/com/lxd/interceptor/WebConfig.java
@@ -1,8 +1,12 @@
 package com.lxd.interceptor;
 
+import org.springframework.boot.system.ApplicationHome;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.io.File;
 
 /**
  * Created by Cris on 2020/3/24
@@ -17,5 +21,17 @@ public class WebConfig implements WebMvcConfigurer {      //采用JavaBean的形
                 .addPathPatterns("/admin/**")      //加过滤的路径
                 .excludePathPatterns("/admin")      //添加排除的文件
                 .excludePathPatterns("/admin/login");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //获取文件的真实路径
+        /*ApplicationHome home = new ApplicationHome(getClass());
+        File sysfile = home.getSource();
+        String jarPath = sysfile.getParentFile().toString();*/
+        String path =  "/usr/img/";
+        String path1 = "path";//System.getProperty("user.dir") + "\\src\\main\\webapp\\uptextimg\\";
+        registry.addResourceHandler("/static/img/**").addResourceLocations("file:" + path);
+        WebMvcConfigurer.super.addResourceHandlers(registry);
     }
 }
--- src/main/java/com/lxd/web/MdController.java
+++ src/main/java/com/lxd/web/MdController.java
@@ -8,8 +8,10 @@ import com.alibaba.fastjson.JSONObject;
 import com.fasterxml.jackson.databind.util.JSONPObject;
 //import org.json.JSONObject;
 import org.apache.commons.io.FileUtils;
+import org.springframework.boot.system.ApplicationHome;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.FileCopyUtils;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,6 +20,8 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/md")
@@ -32,22 +36,64 @@ public class MdController {
         try {
             request.setCharacterEncoding("UTF-8");
             response.setHeader("Content-Type", "text/html");
+            ApplicationHome home = new ApplicationHome(getClass());     //下面三句获取jar所在父路径
+            File sysfile = home.getSource();
+            String jarPath = sysfile.getParentFile().toString();
+            String path = jarPath + "/img/";
+            //String path1 = "/usr/img";
+            System.out.println("上传图片到"+path);
+            File rootpath = new File(path);
+            if(!rootpath.exists()){
+                rootpath.mkdirs();
+            }
+            String uuid = UUID.randomUUID().toString();
+            File realFile = new File(rootpath + File.separator + uuid+attach.getOriginalFilename());
+            FileUtils.copyInputStreamToFile(attach.getInputStream(),realFile);
+            jsonObject.put("success", 1);
+            jsonObject.put("message", "上传成功");
+            jsonObject.put("url", "http://www.cris15.top/static/img/"+uuid+attach.getOriginalFilename());    //访问这个路径会去真实的路径下面找
+        }
+        catch (Exception e) {
+            jsonObject.put("success", 0);
+        }
+        return jsonObject;
+    }
+
+
+    /*@RequestMapping("/uploadImg")
+    @ResponseBody
+    public JSONObject uploadImg(HttpServletRequest request, HttpServletResponse response,
+                                @RequestParam(value = "editormd-image-file", required = false) MultipartFile attach)
+    {
+        JSONObject jsonObject=new JSONObject();
+        try {
+            request.setCharacterEncoding("UTF-8");
+            response.setHeader("Content-Type", "text/html");
             String path = request.getSession().getServletContext().getRealPath("/static/img/");
             System.out.println("上传图片到"+path);
             File rootpath = new File(path);
             if(!rootpath.exists()){
                 rootpath.mkdirs();
             }
-            File realFile = new File(rootpath + File.separator + attach.getOriginalFilename());
+            String uuid = UUID.randomUUID().toString();
+            File realFile = new File(rootpath + File.separator + uuid+attach.getOriginalFilename());
             FileUtils.copyInputStreamToFile(attach.getInputStream(),realFile);
             jsonObject.put("success", 1);
             jsonObject.put("message", "上传成功");
-            jsonObject.put("url", "/static/img/"+attach.getOriginalFilename());
+            jsonObject.put("url", "http://www.cris15.top/static/img/"+uuid+attach.getOriginalFilename());
         }
         catch (Exception e) {
             jsonObject.put("success", 0);
         }
         return jsonObject;
+    }*/
+
+    @RequestMapping("test")
+    public void getpath(HttpServletRequest request) throws FileNotFoundException {
+        ApplicationHome home = new ApplicationHome(getClass());
+        File sysfile = home.getSource();
+        String jarPath = sysfile.toString();
+        System.out.println();
     }
 
 }
